ecc82bce0427347933a8b3acdfddd2c86a4dd201
unhappychoice <unhappychoice@gmail.com>
2017-03-07 04:42:16 +0900

    change output form

--- lib/intercom_summary.rb
+++ lib/intercom_summary.rb
@@ -9,7 +9,6 @@ module IntercomSummary
     def self.start
       users = []
 
-      puts 'Loading users from intercom...'
       intercom = Intercom::Client.new(app_id: ENV['INTERCOM_APP_ID'], api_key: ENV['INTERCOM_APP_KEY'])
       intercom.users.scroll.each { |user| users.push user }
 
@@ -17,14 +16,9 @@ module IntercomSummary
       weekly_active_users = users.select { |user| user.last_request_at.to_i + SECONDS_OF_DAY * 7 > Time.now.to_i }
       daily_active_users = users.select { |user| user.last_request_at.to_i + SECONDS_OF_DAY * 1 > Time.now.to_i }
 
-      puts '====================================='
-      puts 'Active Users'
-      puts '====================================='
-      puts "monthly: #{monthly_active_users.size}"
-      puts "weekly: #{weekly_active_users.size}"
-      puts "daily: #{daily_active_users.size}"
-
-      puts ''
+      puts "Monthly Active Users: #{monthly_active_users.size}"
+      puts "Weekly Active Users: #{weekly_active_users.size}"
+      puts "Daily Active Users: #{daily_active_users.size}"
     end
   end
 end
