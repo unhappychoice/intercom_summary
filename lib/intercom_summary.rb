61b45c0b3e3c9701631d3c0f21fcd6c7f1e69bbf
Yuji Ueki <unhappychoice@gmail.com>
2017-03-16 00:35:24 +0900

    add team summaries

--- lib/intercom_summary.rb
+++ lib/intercom_summary.rb
@@ -5,26 +5,43 @@ require 'time'
 module IntercomSummary
   class CLI
     def self.start
-      users = []
-
       intercom = Intercom::Client.new(app_id: ENV['INTERCOM_APP_ID'], api_key: ENV['INTERCOM_APP_KEY'])
+
+      # User summaries
+      users = []
       intercom.users.scroll.each { |user| users.push user }
 
-      monthly_active_users = users.select { |user| is_date_before_days(user.last_request_at, 30) }
-      weekly_active_users = users.select { |user| is_date_before_days(user.last_request_at, 7) }
-      daily_active_users = users.select { |user| is_date_before_days(user.last_request_at, 1) }
+      puts "Monthly active users\t\t#{filter_by_last_request_at(users, 30).size}"
+      puts "Weekly active users\t\t#{filter_by_last_request_at(users, 7).size}"
+      puts "Daily active users\t\t#{filter_by_last_request_at(users, 1).size}"
 
-      monthly_signed_up_users = users.select { |user| is_date_before_days(user.signed_up_at, 30) }
-      weekly_signed_up_users = users.select { |user| is_date_before_days(user.signed_up_at, 7) }
-      daily_signed_up_users = users.select { |user| is_date_before_days(user.signed_up_at, 1) }
+      puts "Monthly signed up users\t\t#{filter_by_signed_up_at(users, 30).size}"
+      puts "Weekly signed up users\t\t#{filter_by_signed_up_at(users, 7).size}"
+      puts "Daily signed up users\t\t#{filter_by_signed_up_at(users, 1).size}"
 
-      puts "Monthly active users\t#{monthly_active_users.size}"
-      puts "Weekly active users\t#{weekly_active_users.size}"
-      puts "Daily active users\t#{daily_active_users.size}"
+      # Company summaries
+      companies = []
+      intercom.companies.scroll.each { |company| companies.push company }
 
-      puts "Monthly signed up users\t#{monthly_signed_up_users.size}"
-      puts "Weekly signed up users\t#{weekly_signed_up_users.size}"
-      puts "Daily signed up users\t#{daily_signed_up_users.size}"
+      puts "Monthly active companies\t#{filter_by_last_request_at(companies, 30).size}"
+      puts "Weekly active companies\t\t#{filter_by_last_request_at(companies, 7).size}"
+      puts "Daily active companies\t\t#{filter_by_last_request_at(companies, 1).size}"
+
+      puts "Monthly signed up companies\t#{filter_by_created_at(companies, 30).size}"
+      puts "Weekly signed up companies\t#{filter_by_created_at(companies, 7).size}"
+      puts "Daily signed up companies\t#{filter_by_created_at(companies, 1).size}"
+    end
+
+    def self.filter_by_last_request_at(list, days)
+      list.select { |e| is_date_before_days(e.last_request_at, days) }
+    end
+
+    def self.filter_by_signed_up_at(list, days)
+      list.select { |e| is_date_before_days(e.signed_up_at, days) }
+    end
+
+    def self.filter_by_created_at(list, days)
+      list.select { |e| is_date_before_days(e.created_at, days) }
     end
 
     def self.is_date_before_days(date, days)
